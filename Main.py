59058790377ee020373e6090fe27b6b447bad554
Felipedmgs <felipedomingos.sun@gmail.com>
2026-04-07 11:08:52 -0300

    feat/main.py & directories

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from routers.tab_router import router
 
 app = FastAPI()
 
-app.run()
+app.include_router(router)
+
+@app.get("/")
+def home():
+    return {"Message": "Sistema para comandas de restaurante com MongoDB e FastAPI"}
